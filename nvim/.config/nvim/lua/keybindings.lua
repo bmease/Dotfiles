0a9784991c207f008fe70571cf3fb5297cae6714
Bradley Mease <bmease@gmail.com>
2021-12-12 19:54:10 -0500

    Use utils nmap

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -1,20 +1,3 @@
--- remap function
--- local function map(mode, shortcut, command)
--- 	vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
--- end
---
--- local function nmap(shortcut, command)
--- 	map("n", shortcut, command)
--- end
---
--- local function imap(shortcut, command)
--- 	map("i", shortcut, command)
--- end
---
--- local function vmap(shortcut, command)
--- 	map("v", shortcut, command)
--- end
-
 utils = require('utils')
 map = utils.map
 nmap = utils.nmap
