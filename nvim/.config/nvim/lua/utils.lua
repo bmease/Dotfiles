74162f555eb2ed6294b9768784523cb15816d0a4
Bradley Mease <bmease@gmail.com>
2022-01-28 20:08:30 -0500

    Style telescope

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -28,5 +28,13 @@ M.tmap = function(shortcut, command)
 	map("t", shortcut, command)
 end
 
+--- Set the foreground and background colors of a highlight group
+---@param group string - Group to set.
+---@param fgcolor string - Foreground color.
+---@param bgcolor string - Background color.
+M.highlight = function(group, fgcolor, bgcolor)
+	vim.cmd('highlight ' .. group .. ' guifg=' .. fgcolor .. ' guibg=' .. bgcolor)
+end
+
 
 return M
