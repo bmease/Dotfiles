74d15212d7d1fa981bf4c6f9091fb751032c1553
Bradley Mease <bmease@gmail.com>
2022-01-29 18:00:03 -0500

    Add gitsigns which-key mappings

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -8,3 +8,29 @@ require('gitsigns').setup({
       delete = { text='▌' }
   }
 })
+
+
+-- Which-key mappings
+local wk = require("which-key")
+
+local nmappings = {
+  ['['] = {
+    c = { "<cmd>Gitsigns next_hunk<CR>", "next hunk" }
+  },
+  [']'] = {
+    c = { "<cmd>Gitsigns prev_hunk<CR>", "previous hunk" }
+  }
+}
+
+local leaderMappings = {
+  h = {
+    s = { "<cmd>Gitsigns stage_hunk<CR>", "stage hunk" },
+    u = { "<cmd>Gitsigns undo_stage_hunk<CR>", "undo stage hunk" },
+    S = { "<cmd>Gitsigns stage_buffer<CR>", "stage buffer" },
+    r = { "<cmd>Gitsigns reset_hunk<CR>", "reset hunk" },
+    R = { "<cmd>Gitsigns reset_buffer<CR>", "reset buffer" }
+  }
+}
+
+wk.register(nmappings, { mode = "n" })
+wk.register(leaderMappings, { prefix = "<leader>", mode = "n" })
